753f0c4f7f8e6a0cc2b7e495a9ae650d72bd6cd2
tejas <tejasshah@gmail.com>
2023-03-26 11:39:00 +0530

    stag 1

---
+++ src/com/stag/GreetTag.java
@@ -0,0 +1,30 @@
+package com.stag;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class GreetTag extends SimpleTagSupport {
+
+	@Override
+	public void doTag() throws JspException, IOException {
+		Calendar c = Calendar.getInstance();
+		int h = c.get(Calendar.HOUR);
+
+		JspWriter out = getJspContext().getOut();
+
+		if (h >= 0 && h <= 11) {
+			out.print("Good Morning");
+		} else if (h >= 12 && h <= 17) {
+			out.print("Good Noon");
+
+		} else {
+			out.print("Good Evening");
+
+		}
+
+	}
+}
